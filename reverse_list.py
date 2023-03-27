cf87483628fe2cf3fc3afec6a5500e5fc424c7b6
bwilkins96 <ben.jr.speed@gmail.com>
2023-03-27 15:43:56 -0400

    reverse list

--- reverse_list.py
+++ reverse_list.py
@@ -2,21 +2,19 @@
 # Recursive function that reverses a list
 
 def reverse_list(lst, start=0, stop=None):
-    '''Reverses the order of a list and accepts optional start and stop arguments'''
+    '''Reverses the order of a list from start to stop indexes'''
     if not stop: stop = len(lst) - 1
 
     if stop - start > 1:
         lst[start], lst[stop] = lst[stop], lst[start]
         reverse_list(lst, start+1, stop-1)
 
-if __name__ == '__main__':
-    test_1 = [1,2,3,4,5]
-    test_2 = [14, 17, 21, 257, 4]
-    test_3 = []
+    return lst
 
-    reverse_list(test_1)   
-    reverse_list(test_2)
-    reverse_list(test_3) 
+if __name__ == '__main__':
+    test_1 = reverse_list([1,2,3,4,5])   
+    test_2 = reverse_list([14, 17, 21, 257, 4])
+    test_3 = reverse_list([]) 
     
     print(test_1)          # -> [5, 4, 3, 2, 1]
     print(test_2)          # -> [4, 257, 21, 17, 14]

0bb73a3f53184a0c256d4ef97dd7fea3de769652
bwilkins96 <ben.jr.speed@gmail.com>
2023-03-29 11:21:04 -0400

    changes

--- min_max.py
+++ min_max.py
@@ -2,11 +2,11 @@
 # Recursive function that finds the minimum and maximum values in a sequence
 
 def min_max(seq, idx=0, min=float('inf'), max=float('-inf')):
-    """Returns the min and max values of a sequence"""
-    if len(seq) == 0: 
-        min, max = None, None    
-    
+    """Returns the min and max values of a sequence"""    
     if idx == len(seq):
+        if len(seq) == 0: 
+            min, max = None, None    
+
         return (min, max)
 
     val = seq[idx]
@@ -18,8 +18,10 @@ def min_max(seq, idx=0, min=float('inf'), max=float('-inf')):
 if __name__ == '__main__':
     test_1 = min_max([1, 2, 3, 4, 5])
     test_2 = min_max([6, 7, -513, 8, 9, 20000, 10])
-    test_3 = min_max([])
+    test_3 = min_max([5])
+    test_4 = min_max([])
     
     print(test_1)          # -> (1, 5)
     print(test_2)          # -> (-513, 20000)
-    print(test_3)          # -> (None, None)
+    print(test_3)          # -> (5, 5)
+    print(test_4)          # -> (None, None)
--- reverse_list.py
+++ reverse_list.py
@@ -5,7 +5,7 @@ def reverse_list(lst, start=0, stop=None):
     """Reverses the order of a list from start to stop indexes"""
     if not stop: stop = len(lst) - 1
 
-    if stop - start > 1:
+    if stop - start >= 1:
         lst[start], lst[stop] = lst[stop], lst[start]
         reverse_list(lst, start+1, stop-1)
 
@@ -14,8 +14,10 @@ def reverse_list(lst, start=0, stop=None):
 if __name__ == '__main__':
     test_1 = reverse_list([1, 2, 3, 4, 5])   
     test_2 = reverse_list([14, 17, 21, 257, 4])
-    test_3 = reverse_list([]) 
+    test_3 = reverse_list([1, 2]) 
+    test_4 = reverse_list([])
     
     print(test_1)          # -> [5, 4, 3, 2, 1]
     print(test_2)          # -> [4, 257, 21, 17, 14]
-    print(test_3)          # -> []
+    print(test_3)          # -> [2, 1]
+    print(test_4)          # -> []
